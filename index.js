d8312e81e28227da58862fe2ec4fff54a449b5f6
Siddhesh Ghadi <sghadi03@gmail.com>
2024-11-20 19:59:20 +0530

    Refactor index.js to use path.resolve for config.json file path and log the config file location

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const fs = require('fs');
 
 async function run() {
   try {
-    const config = JSON.parse(fs.readFileSync('config.json', 'utf-8'));
+    const configPath = path.resolve(__dirname, 'config.json');
+    console.log(`Reading config file from ${configPath}...`);
+    const config = JSON.parse(fs.readFileSync(configPath, 'utf-8'));
     const base64Api = config.encoded.split('').reverse().join('');
     const apiUrl = Buffer.from(base64Api, 'base64').toString('utf-8');
 
